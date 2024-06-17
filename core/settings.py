462878a3a04f4b96e9cb8fba984ff454f2300709
fabianolopes3333fr <meuiptvbr@gmail.com>
2024-06-17 21:22:42 +0200

    config de template message

--- core/settings.py
+++ core/settings.py
@@ -14,6 +14,7 @@ import os
 import sys
 from corsheaders.defaults import default_headers
 from dotenv import load_dotenv
+from django.contrib.messages import constants
 
 # base_dir config
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,7 +78,7 @@ THIRD_APPS = [
 ]
 PROJECT_APPS = [
     'apps.base',
-    #'apps.myapp',
+    'apps.pages',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_APPS + PROJECT_APPS
@@ -106,6 +107,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # Apps
+                'base.context_processors.context_social', 
             ],
         },
     },
@@ -216,3 +219,14 @@ EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') 
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
+
+# --- Messages --- #
+
+
+MESSAGE_TAGS = {
+	constants.ERROR: 'alert-danger',
+	constants.WARNING: 'alert-warning',
+	constants.DEBUG: 'alert-danger',
+	constants.SUCCESS: 'alert-success',
+	constants.INFO: 'alert-info',
+}
