2d70b2773b6e615d16806eb4d00a430f1123cb6e
fabianolopes3333fr <meuiptvbr@gmail.com>
2024-06-29 21:09:07 +0200

    config de perfil 3

--- apps/perfil/views.py
+++ apps/perfil/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render
 from contas.models import MyUser
 
-def perfil_view(request, id):
-    perfil = get_object_or_404(MyUser.objects.select_related('perfil'), id=id)
+def perfil_view(request, username):
+    perfil = get_object_or_404(MyUser.objects.select_related('perfil'), username=username)
     context = {'obj': perfil}
     return render(request, 'perfil.html', context)
